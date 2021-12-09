13778e8f640d6c3b64d2e38b19762516772fb739
oxbey <qqaa_fds@ukr.net>
2021-12-09 17:32:14 +0200

    start

---
+++ src/main/java/org/logos/App.java
@@ -0,0 +1,13 @@
+package org.logos;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
